6fac19821c4b4416d6615888168ddeb7f8163441
Raul Ciurescu <raul.ciurescu@eu.rsystems.com>
2023-06-15 19:44:49 +0300

    chore: change metrics var types

--- src/main/java/com/example/mcpclient/model/Metrics.java
+++ src/main/java/com/example/mcpclient/model/Metrics.java
@@ -1,12 +1,12 @@
 package com.example.mcpclient.model;
 
 public record Metrics(
-        int numberOfRowsWithMissingFields,
-        int numberOfMessagesWithBlankContent,
-        int numberOfRowsWithFieldsErrors,
-        int numberOfCallsByCountryCode,
-        int relationBetweenOkKoCalls,
-        int averageCallDurationGroupedByCountryCode,
-        int wordOcurrenceRankingInMessageContentField
+        long numberOfRowsWithMissingFields,
+        long numberOfMessagesWithBlankContent,
+        long numberOfRowsWithFieldsErrors,
+        long numberOfCallsByCountryCode,
+        long relationBetweenOkKoCalls,
+        long averageCallDurationGroupedByCountryCode,
+        long wordOcurrenceRankingInMessageContentField
 ) {
 }
