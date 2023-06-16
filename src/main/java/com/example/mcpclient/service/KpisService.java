b5d54a2173587164ebfc8e3217c0989b06da76aa
Raul Ciurescu <raul.ciurescu@eu.rsystems.com>
2023-06-16 14:45:43 +0300

    min refactor for kpis service

--- src/main/java/com/example/mcpclient/service/KpisService.java
+++ src/main/java/com/example/mcpclient/service/KpisService.java
@@ -1,12 +1,22 @@
 package com.example.mcpclient.service;
 
 import com.example.mcpclient.model.Kpis;
+import com.example.mcpclient.model.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class KpisService {
+    //    Total number of processed JSON files
+    //    Total number of rows
+    //    Total number of calls
+    //    Total number of messages
+    //    Total number of different origin country codes (https://en.wikipedia.org/wiki/MSISDN)
+    //    Total number of different destination country codes (https://en.wikipedia.org/wiki/MSISDN)
+    //    Duration of each JSON process
     private long numberOfProcessedJsonFiles;
     private long totalNumberOfRows;
     private long totalNumberOfCalls;
@@ -16,6 +26,9 @@ public class KpisService {
     private long durationOfEachJsonProcess;
 
     public Kpis getCounters() {
+        log.debug("KpisService.getCounters()");
+        List<Message> messages = ClientJsonService.messages;
+
 
         return new Kpis(
                 numberOfProcessedJsonFiles,
